fb8b715ba676ed379947914a5eb6c274fa9eb9dd
sleepy-pan-da <cheeyang85@gmail.com>
2021-09-01 10:49:15 +0800

    setting up git

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class MyIntEvent : UnityEvent<int>
+{
+}
+
+public class TestScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject cubePrefab;
+    [SerializeField]
+    private MyIntEvent TestEvent;
+    private String MessageToPrint = "Sup boi";
+
+    private int currentXCoodinate = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(cubePrefab, new Vector3(currentXCoodinate, 0, 0), Quaternion.identity);
+            currentXCoodinate += 1;
+            TestEvent.Invoke(currentXCoodinate);
+        }
+    }
+
+    public void PrintMessage(int numberPassed)
+    {
+        Debug.Log(MessageToPrint + " " + numberPassed.ToString());
+    }
+}
